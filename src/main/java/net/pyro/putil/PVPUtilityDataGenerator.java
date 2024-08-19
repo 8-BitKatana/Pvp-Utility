e832c3e697ecb56e0c1901d3a0914c1f4d19a7d7
PopRocks655 <139496309+PopRocks655@users.noreply.github.com>
2024-08-19 14:40:30 -0400

    Initial commit

---
+++ src/main/java/net/pyro/putil/PVPUtilityDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pyro.putil;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PVPUtilityDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/pyro/putil/PVPUtiliyClient.java
@@ -0,0 +1,10 @@
+package net.pyro.putil;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PVPUtiliyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
